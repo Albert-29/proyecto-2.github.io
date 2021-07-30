7d9331869d4e2c4dc1b18bfd6563056a89265203
Alberto Miranda <albert59.gs@gmail.com>
2021-07-30 12:29:40 -0500

    creating the delete function

--- scripts.js
+++ scripts.js
@@ -15,22 +15,73 @@ const CrearAlumno = (nombre) => {
 }
 const GuardarAlumno = () => {
     localStorage.setItem('lista', JSON.stringify(arrayAlumnos));
+    VerAlumnos();
+} /* Los arrays no se pueden guardar dentro del localStorage es por eso que se utiliza la función JSON.stringify para convertirlos a strings */
+    
+
+const VerAlumnos = () => {
+    listaAlumnosUI.innerHTML = '';
+
+    arrayAlumnos = JSON.parse(localStorage.getItem('lista'));
+
+    if (arrayAlumnos === null){
+        arrayAlumnos = [];
+    }else{
+
+        arrayAlumnos.forEach(element => {
+            listaAlumnosUI.innerHTML += `<div class="alert alert-primary d-flex justify-content-evenly align-items-center  flex-wrap" role="alert"><i class="fas fa-users me-3"></i><b>${element.nombre}</b><span class="d-flex"><i class="fas fa-user-edit me-4">edit</i><i class="fas fa-user-minus me-0">delete</i></span></div>`
+        });
+    }
 }
-/* Los arrays no se pueden guardar dentro del localStorage es por eso que se utiliza la función JSON.stringify para convertirlos a strings */
+const Eliminar = (nombre) => {
+    let indexArray;
+    arrayAlumnos.forEach((elemento, index) => {
+        if(elemento.nombre === nombre){
+            indexArray  = index;
+        }
+    });
+    arrayAlumnos.splice(indexArray,1);
+    GuardarAlumno();
+}
+/*   let indexAlumnos;
+    arrayAlumnos.forEach((elemento,index) => {
+        if(elemento.lista === listaAlumnos){
+            indexAlumnos = index;
+        }
+    }); */
 
-/* let test = CrearAlumno('Alberto Miranda');
-console.log(test) */
-
+    
 //EventListener
 
 formularioUI.addEventListener('submit', (e) => {
     e.preventDefault();
-    let actividadUI = document.querySelector('#names').value;
+    let listaUI = document.querySelector('#names').value;
 
-    CrearAlumno(actividadUI);
+    CrearAlumno(listaUI);
 
     formularioUI.reset(); //nos permite resetar el formulario cada vez que damos enter o click es decir se limpia el campo del formulario
 
     GuardarAlumno();
 
 });
+
+document.addEventListener('DOMContentLoaded', VerAlumnos)
+
+listaAlumnosUI.addEventListener('click', (e) => {
+    
+    e.preventDefault(); 
+
+/*     console.log(e.path[2].childNodes[1].innerHTML); 
+    console.log(e.target.innerHTML) */
+    //.path[0].childNodes[1].innerHTML
+
+    if(e.target.innerHTML === 'edit' || e.target.innerHTML === 'delete'){
+        let texto = e.path[2].childNodes[1].innerHTML;
+        if(e.target.innerHTML === 'delete'){
+            Eliminar(texto)
+        }
+        if(e.target.innerHTML === 'edit'){
+
+        }
+    } 
+}); 
